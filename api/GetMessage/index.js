52f3c9850494f48676ed4a0b966e02f22cffd24f
Aaron Cooper <acooper@ceiamerica.com>
2020-06-26 13:11:43 -0400

    Change API response to JSON

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: "API time"
+        body: JSON.stringify("API time")
     };
 }
